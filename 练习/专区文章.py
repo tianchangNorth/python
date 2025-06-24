8a2ee4bdbe5f01b1e5e3aef8379126f2e5a2721a
tianchangNorth <116717451+tianchangNorth@users.noreply.github.com>
2025-06-24 14:13:44 +0800

    feat:20250624

--- 练习/专区文章.py
+++ 练习/专区文章.py
@@ -1,12 +1,12 @@
 import requests
-url = "https://openatom.atomgit.com/explore/journalism/detail/459723510017822720"
+url = "https://vcs.zijieapi.com/vc/setting?aid=2608&repoId=56081"
 count = 0
 
 def main():
   global count 
   r = requests.post(url)
-  print(count)
+  print(r.text)
   count += 1
 
-while True:
+if __name__ == "__main__":
   main()
